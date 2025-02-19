0a2f90917670fb157310579f1a92b98c90e3e3b0
sakroid <sakroid0507@gmail.com>
2025-02-20 00:12:46 +0900

    Update prototypes_controller.rb

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
   def index
     @prototypes = Prototype.includes(:user)
@@ -19,6 +20,9 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
+    unless current_user.id == @prototype.user_id
+      redirect_to root_path
+    end
   end
 
   def update
